238e362b50a8734cfa99b2e2856a70f6d7868761
sirazhussain <sirazkhan39@outlook.com>
2025-01-07 18:57:45 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'X-Robots-Tag': 'index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1'
+    }
+  }
 })
